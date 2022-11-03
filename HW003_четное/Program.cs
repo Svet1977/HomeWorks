ae599d69a2aca42560e42db4f267c7ff2bd79eb6
Svetlana <Mse1977@bk.ru>
2022-11-04 01:05:34 +0300

    Четность числа

---
+++ HW003_четное/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = new Random().Next(-100,100);
+if (a%2==0)
+{
+    Console.Write(a);
+    Console.WriteLine("- четное число");
+}
+else
+{
+    Console.Write(a);
+    Console.WriteLine("- нечетное число");
+}
