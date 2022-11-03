bf2dad22bd05626a61affa00f0169126e2e047be
Svetlana <Mse1977@bk.ru>
2022-11-03 21:53:04 +0300

    Задача:маx из трех чисел

---
+++ HW001_max _3/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = new Random().Next(1, 100);
+Console.WriteLine(a);
+int b = new Random().Next(1, 100);
+Console.WriteLine(b);
+int c = new Random().Next(1, 100);
+Console.WriteLine(c);
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.Write("max   ");
+Console.WriteLine(max);
+
