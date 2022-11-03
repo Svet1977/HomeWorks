9a03217d9113ebe2cad12382685416b886b92797
Svetlana <Mse1977@bk.ru>
2022-11-04 00:03:55 +0300

    Max и min из двух чисел

--- HW001_max _3/Program.cs
+++ HW001_max _3/Program.cs
@@ -5,9 +5,7 @@ Console.WriteLine(b);
 int c = new Random().Next(-100, 100);
 Console.WriteLine(c);
 int max = a;
-if (a > max) max = a;
 if (b > max) max = b;
 if (c > max) max = c;
 Console.Write("max   ");
 Console.WriteLine(max);
-
