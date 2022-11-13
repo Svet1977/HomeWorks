d282685153b249bc492e44eb73fc1bf389469211
Svetlana <Mse1977@bk.ru>
2022-11-13 19:39:19 +0300

    кубы от 1 до n

---
+++ HW009/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 23. Напишите программу, которая принимает на вход число (N)
+// и выдает таблицу кубов чисел от 1 до N.
+
+int N = new Random().Next(1, 20);
+Console.Write("Выбранный интервал от 1 до ");
+Console.WriteLine(N);
+int result=0;
+
+for(int b=1;b<=N; b++)
+{
+    result=b*b*b;
+    Console.Write($"{result}  ");
+}
